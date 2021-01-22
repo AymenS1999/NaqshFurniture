71c3240b24277c64a229dfe85c9b0fc4bedfb272
AymenS1999 <76783458+AymenS1999@users.noreply.github.com>
2021-01-22 09:29:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 window.addEventListener('scroll', function(){
 	const header = document.querySelector('header');
 	header.classList.toggle("sticky", window.scrollY > 100);
+	for(i=0; i<document.querySelectorAll('.sub-menu').length; i++)
+		document.querySelectorAll('.sub-menu')[i].classList.toggle("sub-menu-onScroll", window.scrollY > 100);
 });
+
