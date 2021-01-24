852b3a2a3f85d109660661850ff4d8c9c14a633a
AymenS1999 <76783458+AymenS1999@users.noreply.github.com>
2021-01-24 19:00:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ window.addEventListener('scroll', function(){
 		document.querySelectorAll('.sub-menu')[i].classList.toggle("sub-menu-onScroll", window.scrollY > 100);
 });
 
+
+$(document).ready(function() {
+  var counters = $(".counter");
+  var countersQuantity = counters.length;
+  var counter = [];
+
+  for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+  }
+
+  var count = function(start, value, id) {
+    var localStart = start;
+    setInterval(function() {
+      if (localStart < value) {
+        localStart++;
+        counters[id].innerHTML = localStart;
+      }
+    }, 0);
+  }
+
+  for (j = 0; j < countersQuantity; j++) {
+    count(4000, counter[j], j);
+  }
+});
